bfafcd6ee4747509d49b92b498a523ad349d02cc
baconYao <bacon735392@gmail.com>
2019-08-13 21:29:02 +0800

    Pass firebase & firestore as arguments

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,7 @@
 export const createProject = (project) => {
   // 由於使用了 thunk package，因此可以return function
-  return (dispatch, getState) => {
+  //  getFirebase, getFirestore 是由index.js的thunk.withExtraArgument({getFirebase, getFirestore} 傳入
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
     // we can make async call to database
     
     dispatch({ type: "CREATE_PROJECT", project});

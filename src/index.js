2ab0a149ae066a4917d22dde07b31d0c1b0392bc
baconYao <bacon735392@gmail.com>
2019-08-28 07:58:06 +0800

    User profile data

--- src/index.js
+++ src/index.js
@@ -11,12 +11,15 @@ import { getFirestore, reduxFirestore } from 'redux-firestore';
 import { getFirebase, reactReduxFirebase } from 'react-redux-firebase';
 import fbConfig from './config/fbConfig';
 
-// withExtraArgument讓我們可以傳入需要使用到的東西，然後再action時使用它們(可以參考store/actions/projectActions.js)
+// withExtraArgument讓我們可以傳入需要使用到的東西，然後在action時使用它們(可以參考store/actions/projectActions.js)
 const store = createStore(rootReducer, 
   compose(
     applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
     reduxFirestore(fbConfig),
-    reactReduxFirebase(fbConfig, {attachAuthIsReady: true})
+    // useFirestoreForProfile 是用來取出使用者存在firestore的資訊(由於我們使用firebase給予的uid當成firestore user collection的document的uid，因此可以對應到正確的user)
+    // userProfile：告訴firestore要使用個collections內的資料當成 user profile
+    // 設定完上述兩者之後，就可以透過state.firebase.profile看到使用者的資訊(未設定前只能看到isEmpty: true, isLoaded: false)
+    reactReduxFirebase(fbConfig, {useFirestoreForProfile: true, userProfile: 'users', attachAuthIsReady: true})
   )
 );
 

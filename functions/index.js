052b4c5d674bb17f2420f01ee3c1c53e437eabf7
baconYao <bacon735392@gmail.com>
2019-08-29 08:09:06 +0800

    Add userJoined Cloud Functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,9 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+// 初始化，使得cloud functions有能力去存取firebase上面的services
 admin.initializeApp(functions.config().firebase);
 
-// 建立一個https.request的trigger，當有收到request時，就會trigger此cloud function
+// 建立一個https.request的trigger，當有收到request時，就會trigger此cloud functions
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello baconYao");
 });
@@ -11,9 +12,9 @@ const createNotification = (notification => {
   return admin.firestore().collection('notifications')
     .add(notification)
     .then(doc => console.log('notification added', doc))
-})
+});
 
-// 建立一個firestore.document的trigger，當有新的project被建立時，就會trigger這個cloud function
+// 建立一個firestore.document的trigger，當有新的project被建立時，就會trigger這個cloud functions
 exports.projectCreated = functions.firestore
   .document('projects/{projectId}')
   .onCreate(doc => {
@@ -26,4 +27,23 @@ exports.projectCreated = functions.firestore
     }
 
     return createNotification(notification);
-})
+});
+
+// 建立一個auth的trigger，當有新的user加入時，就會trigger此cloud functions
+exports.userJoined = functions.auth.user()
+  .onCreate(user => {
+    
+    return admin.firestore().collection('users')
+      .doc(user.uid).get().then(doc => {
+
+        const newUser = doc.data();
+        const notification = {
+          content: 'Joined the party',
+          user: `${newUser.firstName} ${newUser.lastName}`,
+          time: admin.firestore.FieldValue.serverTimestamp()
+        };
+
+        return createNotification(notification);
+
+      });
+});
